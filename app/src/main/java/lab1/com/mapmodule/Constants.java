3fc6c42c9386b96816e179ca995af22e1a4230a9
Eokinyi35 <emmanuel.okinyi@strathmore.edu>
2017-12-19 17:46:46 +0300

    first commit

---
+++ app/src/main/java/lab1/com/mapmodule/Constants.java
@@ -0,0 +1,10 @@
+package lab1.com.mapmodule;
+
+/**
+ * Created by Eokinyi on 12/19/2017.
+ */
+
+public class Constants {
+    public static final String PUBNUB_PUBLISH_KEY = "pub-c-36625598-59fa-4262-8b90-ef09c8b4d554";
+    public static final String PUBNUB_SUBSCRIBE_KEY = "sub-c-85016e18-e4bb-11e7-9723-66707ad51ffc";
+}
